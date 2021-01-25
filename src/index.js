681ba74d836c1847979d7c7f7464c046f34ab67c
Beauty Spot <75646530+BeautySpot88@users.noreply.github.com>
2021-01-25 01:13:59 +0000

    reorganized layout in index and added forecasts

--- src/index.js
+++ src/index.js
@@ -25,9 +25,8 @@ function dateTime(date) {
   return `Last updated: ${day} ${hour}:${minute}`;
   
 }
-
+ //Celcius temp info display
 function currentTemperature(response) {
-  //Celcius temp display
   document.querySelector("#cityDisplay").innerHTML = response.data.name;
   document.querySelector(".forecast-descrip").innerHTML = response.data.weather[0].description;
   document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp) + "˚C";
@@ -49,43 +48,42 @@ function currentTemperature(response) {
 }
 
 function getForecast (response){
-console.log(response);
+let forecastElementHours = document.querySelector(".upcoming-temps");
+forecastElementHours.innerHTML = null;
+let forecastHours = null;
+let forecastElementDays = document.querySelector(".upcoming-days");
+forecastElementDays.innerHTML = null;
+let forecastDays = null;
+
+for (let i = 1; i < 7; i++) {
+  forecastHours = response.data.hourly[i];
+  forecastElementHours.innerHTML += ` <div class="col" id="hourly-${i}"><h4>${Math.round(forecastHours.temp)}˚C</h4>
+            ${forecastHour(forecastHours.dt)}</div>`;
+}
+
+for (let i = 1; i < 6; i++) {
+  forecastDays = response.data.daily[i];
+  forecastElementDays.innerHTML += ` <div class="col">
+            <h4>${forecastDate(forecastDays.dt)}</h4>
+            <img src="https://openweathermap.org/img/wn/${forecastDays.weather[0].icon}@2x.png" class="icons" />
+            <h4 class="hi-lo"><span class="plus-hi" id="high-${i}">${Math.round(forecastDays.temp.max)}˚C</span> |
+            <span class="plus-lo" id="lo-${i}">${Math.round(forecastDays.temp.min)}˚C</span></h4> 
+          </div>`;
+}
 document.querySelector(".today-hi").innerHTML = Math.round(response.data.daily[0].temp.max) + "˚C";
 document.querySelector(".today-lo").innerHTML = Math.round(response.data.daily[0].temp.min) + "˚C";
-document.querySelector(".plus1-hi").innerHTML = Math.round(response.data.daily[1].temp.max) + "˚C";
-document.querySelector(".plus1-lo").innerHTML = Math.round(response.data.daily[1].temp.min) + "˚C";
-document.querySelector(".plus2-hi").innerHTML = Math.round(response.data.daily[2].temp.max) + "˚C";
-document.querySelector(".plus2-lo").innerHTML = Math.round(response.data.daily[2].temp.min) + "˚C";
-document.querySelector(".plus3-hi").innerHTML = Math.round(response.data.daily[3].temp.max) + "˚C";
-document.querySelector(".plus3-lo").innerHTML = Math.round(response.data.daily[3].temp.min) + "˚C";
-document.querySelector(".plus4-hi").innerHTML = Math.round(response.data.daily[4].temp.max) + "˚C";
-document.querySelector(".plus4-lo").innerHTML = Math.round(response.data.daily[4].temp.min) + "˚C";
-document.querySelector(".plus5-hi").innerHTML = Math.round(response.data.daily[5].temp.max) + "˚C";
-document.querySelector(".plus5-lo").innerHTML = Math.round(response.data.daily[5].temp.min) + "˚C";
-document.querySelector("#iconTomorrow").setAttribute("src",`https://openweathermap.org/img/wn/${response.data.daily[1].weather[0].icon}@2x.png`);
-document.querySelector("#iconPlus1").setAttribute("src",`https://openweathermap.org/img/wn/${response.data.daily[2].weather[0].icon}@2x.png`);
-document.querySelector("#iconPlus2").setAttribute("src",`https://openweathermap.org/img/wn/${response.data.daily[3].weather[0].icon}@2x.png`);
-document.querySelector("#iconPlus3").setAttribute("src",`https://openweathermap.org/img/wn/${response.data.daily[4].weather[0].icon}@2x.png`);
-document.querySelector("#iconPlus4").setAttribute("src",`https://openweathermap.org/img/wn/${response.data.daily[5].weather[0].icon}@2x.png`);
-
-document.querySelector("#forecastDay1").innerHTML = forecastDate(response.data.daily[2].dt);
-document.querySelector("#forecastDay2").innerHTML = forecastDate(response.data.daily[3].dt);
-document.querySelector("#forecastDay3").innerHTML = forecastDate(response.data.daily[4].dt);
-document.querySelector("#forecastDay4").innerHTML = forecastDate(response.data.daily[5].dt);
-
-document.querySelector("#hourplus1").innerHTML = forecastHour(response.data.hourly[1].dt);
-document.querySelector("#hourplus2").innerHTML = forecastHour(response.data.hourly[2].dt);
-document.querySelector("#hourplus3").innerHTML = forecastHour(response.data.hourly[3].dt);
-document.querySelector("#hourplus4").innerHTML = forecastHour(response.data.hourly[4].dt);
-document.querySelector("#hourplus5").innerHTML = forecastHour(response.data.hourly[5].dt);
-
-document.querySelector("#tempNow").innerHTML = Math.round(response.data.hourly[0].temp) + "˚C";
-document.querySelector("#tempHour1").innerHTML = Math.round(response.data.hourly[1].temp) + "˚C";
-document.querySelector("#tempHour2").innerHTML = Math.round(response.data.hourly[2].temp) + "˚C";
-document.querySelector("#tempHour3").innerHTML = Math.round(response.data.hourly[3].temp) + "˚C";
-document.querySelector("#tempHour4").innerHTML = Math.round(response.data.hourly[4].temp) + "˚C";
-document.querySelector("#tempHour5").innerHTML = Math.round(response.data.hourly[5].temp) + "˚C";
-
+highTemp = response.data.daily[0].temp.max;
+lowTemp = response.data.daily[0].temp.min;
+high1 = response.data.daily[1].temp.max;
+low1 = response.data.daily[1].temp.min;
+high2 = response.data.daily[2].temp.max;
+low2 = response.data.daily[2].temp.min;
+high3 = response.data.daily[3].temp.max;
+low3 = response.data.daily[3].temp.min;
+high4 = response.data.daily[4].temp.max;
+low4 = response.data.daily[4].temp.min;
+high5 = response.data.daily[4].temp.max;
+low5 = response.data.daily[4].temp.min;
 
 function forecastDate(date) {
   let days = [
@@ -112,7 +110,7 @@ if (hour < 10) {
     hour = `0${hour}`;
   }
 
-  return hour;
+  return `${hour}:00`;
 }
 }
 
@@ -125,9 +123,11 @@ function search(city) {
 //Searched city name display
 function handleSubmit(event) {
   event.preventDefault();
+  toCelcius();
   let city = document.querySelector("#searchInput").value;
   search(city);
   today.innerHTML = dateTime(new Date());
+
 }
 
 //Current location
@@ -141,6 +141,7 @@ function searchLocation(position) {
 
 function getCurrentLocation(event) {
   event.preventDefault();
+  toCelcius();
   navigator.geolocation.getCurrentPosition(searchLocation);
   today.innerHTML = dateTime(new Date());
 }
@@ -148,6 +149,28 @@ function getCurrentLocation(event) {
 function toFahrenheit(event){
 let fahrenheit = (temperature * 9/5) + 32;
 document.querySelector("#temperature").innerHTML = `${Math.round(fahrenheit)}˚F`;
+
+let highTempConvert = (highTemp * 9/5) + 32;
+document.querySelector(".today-hi").innerHTML = `${Math.round(highTempConvert)}˚F`
+let lowTempConvert = (lowTemp * 9/5) + 32;
+document.querySelector(".today-lo").innerHTML = `${Math.round(lowTempConvert)}˚F`
+let day1HighTemp = (high1 * 9/5) + 32;
+document.querySelector("#high-1").innerHTML = `${Math.round(day1HighTemp)}˚F`
+let day1LowTemp = (low1 * 9/5) + 32;
+document.querySelector("#lo-1").innerHTML = `${Math.round(day1LowTemp)}˚F`
+let day2HighTemp = (high2 * 9/5) + 32;
+document.querySelector("#high-2").innerHTML = `${Math.round(day2HighTemp)}˚F`
+let day2LowTemp = (low2 * 9/5) + 32;
+document.querySelector("#lo-2").innerHTML = `${Math.round(day2LowTemp)}˚F`
+let day3HighTemp = (high3 * 9/5) + 32;
+document.querySelector("#high-3").innerHTML = `${Math.round(day3HighTemp)}˚F`
+let day3LowTemp = (low3 * 9/5) + 32;
+document.querySelector("#lo-3").innerHTML = `${Math.round(day3LowTemp)}˚F`
+let day4HighTemp = (high4 * 9/5) + 32;
+document.querySelector("#high-4").innerHTML = `${Math.round(day4HighTemp)}˚F`
+let day4LowTemp = (low4 * 9/5) + 32;
+document.querySelector("#lo-4").innerHTML = `${Math.round(day4LowTemp)}˚F`
+
 convertToFahrenheit.classList.add("active");
 convertToCelcius.classList.remove("active");
 
@@ -161,6 +184,19 @@ convertToFahrenheit.classList.remove("active");
 }
 
 let temperature = null;
+let highTemp = null;
+let lowTemp = null;
+let high1 = null;
+let low1 = null;
+let high2 = null;
+let low2 = null;
+let high3 = null;
+let low3 = null;
+let high4 = null;
+let low4 = null;
+let high5 = null;
+let low5 = null;
+
 
 let today = document.querySelector("#today");
 today.innerHTML = dateTime(new Date());
